10fb42d56c7ac739c66ee3529f432b7e6c5756b5
CarlosJardonIglesias <carlosjardon12@gmail.com>
2021-03-06 14:30:39 +0100

    first commit with strings

---
+++ string operations.py
@@ -0,0 +1,17 @@
+hi = "hello"
+print(hi[2])
+print("Hello"[2])
+if hi[2] == hi[3]:
+    print("This is true because we have double l")
+greetings ="hi " + "how " + "are " + "you " + "?"
+print(greetings)
+# multiplication
+print("bye " *2)
+if "hi" > "HI":
+    print("hi > HI")
+else:
+    print("hi < HI")
+#lower casses come after capital letters. The order is numbers, upper case and lower case.
+print("hellooo" > "hello")
+print("ana" in "banana")
+
